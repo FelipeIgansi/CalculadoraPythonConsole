baaf62504228f6ab024322346e8e92052a336d76
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-07-20 23:20:28 -0300

    segundo commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import  operacoes
-
+import  menu
 Operacoes = operacoes
+
+menu.ExibirMenu()
 
 print(Operacoes.SomaDoisNumeros(1, 2))
--- menu.py
+++ menu.py
@@ -1,1 +1,6 @@
-def 
+class Menu:
+
+    def exibirMenu(self):
+
+        print('=====  Menu  =====')
+        print('     Opções:')
