6bae01dfc2eb6da40c022eea51b0311cc56176a6
FelipeIgansi <felipeigansi2009@hotmail.com>
2020-07-20 23:11:49 -0300

    Primeiro commit

---
+++ main.py
@@ -0,0 +1,5 @@
+import  operacoes
+
+Operacoes = operacoes
+
+print(Operacoes.SomaDoisNumeros(1, 2))
---
+++ operacoes.py
@@ -0,0 +1,13 @@
+def SomaDoisNumeros(v1, v2):
+    return v1+v2
+
+def SubtraiDoisNumeros(v1, v2):
+    return v1+v2
+
+
+def MultiplicaDoisNumeros(v1, v2):
+    return v1+v2
+
+    
+def DividirDoisNumeros(v1, v2):
+    return v1+v2
